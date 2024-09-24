415db4725fbaf74603cb200581974d7502a24040
Rodulfo0923 <125709817+Rodulfo0923@users.noreply.github.com>
2024-09-24 18:37:32 +0800

    Update config.php

--- connection/config.php
+++ connection/config.php
@@ -1,12 +1,13 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'notesapp');
+   $conn = mysqli_connect(
+        getenv("DB_HOST"),
+        getenv("DB_USERNAME"),
+        getenv("DB_PASSWORD"),
+        getenv("DB_DATABASE"),
+        getenv("DB_PORT") ? : "3306"
+    );
 
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+  if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 ?>
